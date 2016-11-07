5cfafd4cbed1ac06472238f28cf2651c785d3871
vladimir <righok@gmail.com>
2016-11-07 22:44:06 +0300

    add Image drag and scale

--- GalaxyMap/MainWindow.xaml.cs
+++ GalaxyMap/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace GalaxyMap
 {
@@ -7,6 +10,47 @@ namespace GalaxyMap
         public MainWindow()
         {
             InitializeComponent();
+            EnableMouseMovements();
+        }
+
+        private Point FirstPoint = new Point();
+        private void EnableMouseMovements()
+        {
+            galaxyImage.MouseLeftButtonDown += (sender, args) =>
+            {
+                FirstPoint = args.GetPosition(this);
+                galaxyImage.CaptureMouse();
+            };
+
+            galaxyImage.MouseWheel += (sender, args) =>
+            {
+                var matrix = galaxyImage.RenderTransform.Value;
+                var mousePoint = args.GetPosition(galaxyImage);
+
+                if (args.Delta > 0)
+                    matrix.ScaleAtPrepend(1.15,1.15,mousePoint.X,mousePoint.Y);
+                else
+                    matrix.ScaleAtPrepend(1/1.15,1/1.15, mousePoint.X, mousePoint.Y);
+
+                var newMatrix = new MatrixTransform(matrix);
+                galaxyImage.RenderTransform = newMatrix;
+            };
+
+            galaxyImage.MouseMove += (sender, args) =>
+            {
+                if (args.LeftButton == MouseButtonState.Pressed)
+                {
+                    var temp = args.GetPosition(this);
+                    var res = new Point(FirstPoint.X - temp.X,FirstPoint.Y - temp.Y);
+
+                    Canvas.SetLeft(galaxyImage, Canvas.GetLeft(galaxyImage) - res.X);
+                    Canvas.SetTop(galaxyImage, Canvas.GetTop(galaxyImage) - res.Y);
+
+                    FirstPoint = temp;
+                }
+            };
+
+            galaxyImage.MouseUp += (sender, args) => { galaxyImage.ReleaseMouseCapture(); };
         }
     }
 }
